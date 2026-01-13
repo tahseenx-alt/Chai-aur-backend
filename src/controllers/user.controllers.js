198623abf7fd68ce1a606ea8b88514a398f00804
Tahseen Anwer <210026946+tahseenx-alt@users.noreply.github.com>
2026-01-13 19:49:30 +0530

    Add registerd Controller And Bug Fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,78 @@
 import {asyncHandler} from "../utils/asynchandler.js"
+import {ApiError} from "../utils/apiError.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {ApiResponse} from "../utils/apiresponse.js"
 
 const registerUser = asyncHandler(async (req,res)=>{
-    return  res.status(200).json({
-        message:"Server is runnig "
+     // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+
+    const {fullname,email,username,paswword} = req.body
+    console.log("email:",email);
+    // other appraoch but here we have to write for all case 
+    /*
+      if(fullname === ""){
+       throw 
+      }
+     */
+    if (
+        [fullname,email,username,paswword].some((field)=>{
+         field?.trim() === ""})
+    ){
+        throw new ApiError(400,"All field must be full ")
+    }
+
+    // checking the user is already exit or not 
+    const exitedUser =  User.findOne = ({
+        $or:[{ username },{ email}]
     })
+    
+    if(exitedUser){
+        throw new ApiError (409,"User with email or username already exists")
+    }
+
+    // images or avatar
+    const avatarLocalPath =  req.files?.avatar[0]?.path;
+    const coverImageLocalPath =   req.files.coverImage[0]?.path;
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required ")
+    }
+    // uploading on Cloundinary
+    const avatar =  await uploadOnCloudinary(avatarLocalPath)
+    const CoverageImage =  await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400,"Avatar file is required ")
+    }
+    const user =  await User.create({
+        fullname,
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        paswword,
+        username:username.toLowerCase()
+    })
+
+    const createdUser =  await User.findById(user._id).select(
+        // removing 
+        "-password -refreshToken"
+    )
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong while registrating the User ")
+    }
+
+    // returning 
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registerd Succesfully ")
+    )
 })
 
 
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-
+import {ApiError} from "../utils/apiError.js"
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./public/temp") // where you wanna keep the file
