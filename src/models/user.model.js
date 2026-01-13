797fb984bb71032dee6757386095b0164b065926
Tahseen Anwer <210026946+tahseenx-alt@users.noreply.github.com>
2026-01-13 22:57:56 +0530

    error solved

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,6 @@ userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next();
 
     this.password = await bcrypt.hash(this.password,10)
-    next()
 })
 
 //custom method for password checking 
